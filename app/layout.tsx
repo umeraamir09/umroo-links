7e74124ce9e100d96671cf6cece52e569ed34cfa
Umer Aamir <130839691+umeraamir09@users.noreply.github.com>
2026-02-26 05:37:16 +0500

    BMAC integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
       >
         {children}
       </body>
+      <script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="umroo" data-description="Support me on Buy me a coffee!" data-message="Thanks for visiting! You can buy me a coffee so I have the energy to make cool stuff for you :)" data-color="#FF5F5F" data-position="Right" data-x_margin="18" data-y_margin="18"></script>
     </html>
   );
 }
